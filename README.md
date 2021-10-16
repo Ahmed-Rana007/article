5a73ce1790be3b60d9cabeee3290886422a484f3
Ahmed-Rana007 <ra03334110241@gmail.com>
2021-10-17 00:58:23 +0500

    del

--- README.md
+++
@@ -1,7 +0,0 @@
-Project : Article 
-Muhammad Ahmed
-l1f17bscs0492
-Sec F
-Usama Safdar
-L1f17bscs0141
-Sec F
--- src/com/hbl/selfservice/UserOperations.java
+++
@@ -1,8 +0,0 @@
-package com.hbl.selfservice;
-
-public class UserOperations {
-	public static String methodJS(String str)
-	{
-		return "Method called: = " + str;
-	}
-}
